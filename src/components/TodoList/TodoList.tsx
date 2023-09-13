3cb29c42dedd55d4a2282d6ce167d0a2e61203ef
Robert Baruck <robert.baruck@sandstorm.de>
2023-09-13 12:18:57 +0200

    TASK: Exercise 6

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,9 +1,14 @@
-import { useEffect, useState } from "react"
+import { useEffect, useId, useState } from "react"
 import TodoListItem from "./TodoListItem"
-import { Todo, fetchTodos } from "../../model/Todo"
+import { Todo, fetchTodos, filterOutCompleted, filterTodosByTitle } from "../../model/Todo"
 
 const TodoList = () => {
     const [todos, setTodos] = useState<Array<Todo>>([])
+    const [filterText, setFilterText] = useState('')
+    const [filterCompleted, setFilterCompleted] = useState(false)
+    
+    const filterTextInputId = useId()
+    const filterCompletedInputId = useId()
 
     useEffect(
         // function with side effects to run
@@ -12,11 +17,29 @@ const TodoList = () => {
         [setTodos],
     )
 
+    const handleFilterTextChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+        setFilterText(e.target.value)
+    }
+
+    const handleFilterCompletedChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+        setFilterCompleted(e.target.checked)
+    }
+
+    const filteredTodos = filterOutCompleted(filterTodosByTitle(todos, filterText), filterCompleted)
+
     return (
         <div>
             <h2>Todos</h2>
+            <div>
+                <label htmlFor={filterTextInputId}>filter: </label>
+                <input id={filterTextInputId} value={filterText} onChange={handleFilterTextChange} />
+            </div>
+            <div>
+                <input id={filterCompletedInputId} type="checkbox" checked={filterCompleted} onChange={handleFilterCompletedChange} />
+                <label htmlFor={filterCompletedInputId}>filter out completed</label>
+            </div>
             <ul>
-                {todos.map(todo => 
+                {filteredTodos.map(todo => 
                     <TodoListItem 
                         key={todo.id} 
                         completed={todo.completed}
--- src/model/Todo.ts
+++ src/model/Todo.ts
@@ -12,3 +12,15 @@ export async function fetchTodos() {
     // here should happen some kind of data validation (e.g. with type guards, zod or ajv)
     return todos as Array<Todo>
 }
+
+export function filterTodosByTitle(todos: Array<Todo>, filterText: string) {
+    return todos.filter(todo => todo.title.toLocaleLowerCase().includes(filterText.toLocaleLowerCase()))
+}
+
+export function filterOutCompleted(todos: Array<Todo>, shouldFilterOutCompletedTodos: boolean) {
+    if (!shouldFilterOutCompletedTodos) {
+        return todos
+    }
+
+    return todos.filter(todo => !todo.completed)
+}
